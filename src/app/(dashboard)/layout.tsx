8cc7e6817b2c6de25533418275ded8d92e375c06
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2024-02-08 10:49:07 +0900

    add darkmode

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,12 +1,18 @@
+import { UserButton } from '@clerk/nextjs';
 import { ReactNode } from 'react';
 
 import Logo from './_components/Logo';
+import ThemeSwitcher from './_components/ThemeSwitcher';
 
 function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex max-h-screen min-h-screen min-w-full flex-col bg-background">
       <nav className="flex h-[60px] items-center justify-between border-b border-border px-4 py-2">
         <Logo />
+        <div className="flex items-center gap-4">
+          <ThemeSwitcher />
+          <UserButton afterSignOutUrl="/sign-in" />
+        </div>
       </nav>
       <main>{children}</main>
     </div>
