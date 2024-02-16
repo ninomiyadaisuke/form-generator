209d3ba83097e67b534b73bc5e0e21eeaf4a8fb1
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2024-02-16 11:31:17 +0900

    view PropertiesComponent

--- src/app/(dashboard)/builder/[id]/_components/DesignerElementWrapper.tsx
+++ src/app/(dashboard)/builder/[id]/_components/DesignerElementWrapper.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useDraggable } from '@dnd-kit/core';
 import React, { useState } from 'react';
 import { BiSolidTrash } from 'react-icons/bi';
 
@@ -14,11 +15,29 @@ import { FormElements } from './FormElements';
 
 const DesignerElementWrapper = ({ element }: { element: FormElementInstance }) => {
   const [mouseIsOver, setMouseIsOver] = useState(false);
-  const { removeElement } = useDesigner();
+  const { removeElement, setSelectedElement } = useDesigner();
+
+  const draggable = useDraggable({
+    id: element.id + '-drag-handler',
+    data: {
+      type: element.type,
+      elementId: element.id,
+      isDesignerElement: true,
+    },
+  });
+
+  if (draggable.isDragging) return null;
+
   const DesignerElement = FormElements[element.type].designerComponent;
   return (
     <div
+      {...draggable.attributes}
+      {...draggable.listeners}
       className="relative flex h-[120px] flex-col rounded-md text-foreground ring-1 ring-inset ring-accent hover:cursor-pointer"
+      onClick={(e) => {
+        e.stopPropagation();
+        setSelectedElement(element);
+      }}
       onMouseEnter={() => setMouseIsOver(true)}
       onMouseLeave={() => setMouseIsOver(false)}
     >
@@ -45,6 +64,7 @@ const DesignerElementWrapper = ({ element }: { element: FormElementInstance }) =
       <div
         className={cn(
           'flex w-full h-[120px] items-center rounded-md bg-accent/40 px-4 py-2 pointer-events-none opacity-100',
+          mouseIsOver && 'opacity-30',
         )}
       >
         <DesignerElement elementInstance={element} />
