bf30394db62cb681489a57e1d434a2d6e6f030ed
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2024-02-20 10:11:47 +0900

    create useDragAndDrop hooks

--- src/app/(dashboard)/builder/[id]/_hooks/index.ts
+++ src/app/(dashboard)/builder/[id]/_hooks/index.ts
@@ -1,8 +1,12 @@
 'use client';
 
+import { DragEndEvent, useDndMonitor, useDroppable } from '@dnd-kit/core';
 import { atom, useAtom } from 'jotai';
 
-import { FormElementInstance } from '../_types';
+import { idGenerator } from '@/lib/utils';
+
+import { FormElements } from '../_components/FormElements';
+import { ElementsType, FormElementInstance } from '../_types';
 
 const elementsContexts = atom<FormElementInstance[]>([]);
 
@@ -42,4 +46,87 @@ export const useDesigner = () => {
     selectedElement,
     setSelectedElement,
   };
+};
+
+export const useDragAndDrop = () => {
+  const { elements, addElement, removeElement } = useDesigner();
+
+  const droppable = useDroppable({
+    id: 'designer-drop-area',
+    data: {
+      isDesignerDropArea: true,
+    },
+  });
+
+  useDndMonitor({
+    onDragEnd: (event: DragEndEvent) => {
+      const { active, over } = event;
+      if (!active || !over) return;
+
+      // First scenario
+      const isDesignerButtonElement = active.data?.current?.isDesignerButtonElement as boolean;
+      const isDroppingOverDesignerDropArea = over.data?.current?.isDesignerDropArea as boolean;
+      const droppingSidebarButtonOverDesignerDropArea = isDesignerButtonElement && isDroppingOverDesignerDropArea;
+
+      if (droppingSidebarButtonOverDesignerDropArea) {
+        const type = active.data.current?.type as ElementsType;
+        const newElement = FormElements[type].construct(idGenerator());
+        addElement(elements.length, newElement);
+      }
+
+      // Second scenario
+      const isDroppingOverDesignerElementTopHalf = over.data?.current?.isTopHalfDesignerElement as boolean;
+      const isDroppingOverDesignerElementBottomHalf = over.data?.current?.isBottomHalfDesignerElement as boolean;
+      const isDroppingOverDesignerElement =
+        isDroppingOverDesignerElementTopHalf || isDroppingOverDesignerElementBottomHalf;
+      const droppingSidebarButtonOverDesingerElement = isDesignerButtonElement && isDroppingOverDesignerElement;
+      if (droppingSidebarButtonOverDesingerElement) {
+        const type = active.data?.current?.type as ElementsType;
+        const newElement = FormElements[type].construct(idGenerator());
+        const overId = over.data?.current?.elementId as string;
+        console.log('overId', overId);
+
+        const overElementIndex = elements.findIndex((el) => el.id === overId);
+
+        if (overElementIndex === -1) {
+          throw new Error('element not found');
+        }
+
+        let indexForNewElement = overElementIndex;
+        if (isDroppingOverDesignerElementBottomHalf) {
+          indexForNewElement = overElementIndex + 1;
+        }
+        addElement(indexForNewElement, newElement);
+        return;
+      }
+      // Third scenario
+
+      const isDraggingDesignerElement = active.data?.current?.isDesignerElement as boolean;
+      const draggingDesignerElementOverAnotherDesignerElement =
+        isDroppingOverDesignerElement && isDraggingDesignerElement;
+
+      if (draggingDesignerElementOverAnotherDesignerElement) {
+        const activeId = active.data?.current?.elementId as string;
+        const overId = over.data?.current?.elementId as string;
+
+        const activeElementIndex = elements.findIndex((el) => el.id === activeId);
+        const overElementIndex = elements.findIndex((el) => el.id === overId);
+        if (activeElementIndex === -1 || overElementIndex === -1) {
+          throw new Error('element not found');
+        }
+
+        const activeElement = { ...elements[activeElementIndex] } as FormElementInstance;
+        removeElement(activeId);
+
+        let indexForNewElement = overElementIndex;
+
+        if (isDroppingOverDesignerElementBottomHalf) {
+          indexForNewElement = overElementIndex + 1;
+        }
+        addElement(indexForNewElement, activeElement);
+      }
+    },
+  });
+
+  return { droppable };
 };
