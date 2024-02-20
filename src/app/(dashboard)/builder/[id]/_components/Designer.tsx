d986a0777ec00e369aac32868af2f24feca19eb2
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2024-02-20 09:23:41 +0900

    create Drag and Drop function

--- src/app/(dashboard)/builder/[id]/_components/Designer.tsx
+++ src/app/(dashboard)/builder/[id]/_components/Designer.tsx
@@ -6,14 +6,14 @@ import React from 'react';
 import { cn, idGenerator } from '@/lib/utils';
 
 import { useDesigner } from '../_hooks';
-import { ElementsType } from '../_types';
+import { ElementsType, FormElementInstance } from '../_types';
 
 import DesignerElementWrapper from './DesignerElementWrapper';
 import DesignerSidebar from './DesignerSidebar';
 import { FormElements } from './FormElements';
 
 const Designer = () => {
-  const { elements, addElement } = useDesigner();
+  const { elements, addElement, selectedElement, setSelectedElement, removeElement } = useDesigner();
   const droppable = useDroppable({
     id: 'designer-drop-area',
     data: {
@@ -25,15 +25,81 @@ const Designer = () => {
     onDragEnd: (event) => {
       const { active, over } = event;
       if (!active || !over) return;
-      const type = active.data.current?.type as ElementsType;
-      const newElement = FormElements[type].construct(idGenerator());
-      addElement(elements.length, newElement);
+
+      // First scenario
+      const isDesignerButtonElement = active.data?.current?.isDesignerButtonElement as boolean;
+      const isDroppingOverDesignerDropArea = over.data?.current?.isDesignerDropArea as boolean;
+      const droppingSidebarButtonOverDesignerDropArea = isDesignerButtonElement && isDroppingOverDesignerDropArea;
+
+      if (droppingSidebarButtonOverDesignerDropArea) {
+        const type = active.data.current?.type as ElementsType;
+        const newElement = FormElements[type].construct(idGenerator());
+        addElement(elements.length, newElement);
+      }
+
+      // Second scenario
+      const isDroppingOverDesignerElementTopHalf = over.data?.current?.isTopHalfDesignerElement as boolean;
+      const isDroppingOverDesignerElementBottomHalf = over.data?.current?.isBottomHalfDesignerElement as boolean;
+      const isDroppingOverDesignerElement =
+        isDroppingOverDesignerElementTopHalf || isDroppingOverDesignerElementBottomHalf;
+      const droppingSidebarButtonOverDesingerElement = isDesignerButtonElement && isDroppingOverDesignerElement;
+      if (droppingSidebarButtonOverDesingerElement) {
+        const type = active.data?.current?.type as ElementsType;
+        const newElement = FormElements[type].construct(idGenerator());
+        const overId = over.data?.current?.elementId as string;
+        console.log('overId', overId);
+
+        const overElementIndex = elements.findIndex((el) => el.id === overId);
+
+        if (overElementIndex === -1) {
+          throw new Error('element not found');
+        }
+
+        let indexForNewElement = overElementIndex;
+        if (isDroppingOverDesignerElementBottomHalf) {
+          indexForNewElement = overElementIndex + 1;
+        }
+        addElement(indexForNewElement, newElement);
+        return;
+      }
+      // Third scenario
+
+      const isDraggingDesignerElement = active.data?.current?.isDesignerElement as boolean;
+      const draggingDesignerElementOverAnotherDesignerElement =
+        isDroppingOverDesignerElement && isDraggingDesignerElement;
+
+      if (draggingDesignerElementOverAnotherDesignerElement) {
+        const activeId = active.data?.current?.elementId as string;
+        const overId = over.data?.current?.elementId as string;
+
+        const activeElementIndex = elements.findIndex((el) => el.id === activeId);
+        const overElementIndex = elements.findIndex((el) => el.id === overId);
+        if (activeElementIndex === -1 || overElementIndex === -1) {
+          throw new Error('element not found');
+        }
+
+        const activeElement = { ...elements[activeElementIndex] } as FormElementInstance;
+        removeElement(activeId);
+
+        let indexForNewElement = overElementIndex;
+
+        if (isDroppingOverDesignerElementBottomHalf) {
+          indexForNewElement = overElementIndex + 1;
+        }
+
+        addElement(indexForNewElement, activeElement);
+      }
     },
   });
 
   return (
     <div className="flex size-full">
-      <div className="w-full p-4">
+      <div
+        className="w-full p-4"
+        onClick={() => {
+          if (selectedElement) setSelectedElement(null);
+        }}
+      >
         <div
           className={cn(
             'bg-background max-w-[920px] h-full m-auto rounded-xl flex flex-col flex-grow items-center justify-start flex-1 overflow-y-auto',
